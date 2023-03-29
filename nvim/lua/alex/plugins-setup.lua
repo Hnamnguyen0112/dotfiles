d3260110da5f7e03a44868af9f038a7742db1211
Alex Nguyen <alex.nguyen@alfagomma.com.sg>
2023-03-29 16:16:07 +0700

    add whiskey

--- nvim/lua/alex/plugins-setup.lua
+++ nvim/lua/alex/plugins-setup.lua
@@ -112,6 +112,19 @@ return packer.startup(function(use)
 
 	use({ "romgrk/barbar.nvim", requires = "nvim-web-devicons" })
 
+	use({
+		"folke/which-key.nvim",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+			require("which-key").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	})
+
 	if packer_bootstrap then
 		require("packer").sync()
 	end
